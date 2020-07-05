b98d1837b956f5e818bf3c8df3a0db967301023b
CoderAlchemy <1395822743@qq.com>
2020-07-05 15:28:34 +0800

    新增用户注册路由

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 require('dotenv').config()
 const Koa = require('koa')
+const bodyParser = require('koa-bodyparser')
+const json = require('koa-json')
 
 const app = new Koa()
 
@@ -7,6 +9,10 @@ const { router } = require('./routers')
 
 const PORT = process.env.PORT
 const HOST = process.env.HOST
+
+app.use(json())
+
+app.use(bodyParser())
 
 app.use(router.routes())
 
--- routers/index.js
+++ routers/index.js
@@ -4,4 +4,23 @@ const router = new Router({
   prefix: '/api'
 })
 
+const formatData = (...data) => {
+  const res = {}
+  data.map((value) => {
+    if (typeof value === 'number') res.code = value
+    if (typeof value === 'string') res.msg = value
+    if (typeof value === 'object') res.data = value
+  })
+  return res
+}
+
+router.post('/register', async (ctx, next) => {
+  try {
+    const { username, password, type } = ctx.request.body
+    ctx.body = formatData(0, { username, password, type })
+  } catch (err) {
+    ctx.body = formatData(1, '服务器错误')
+  }
+})
+
 exports.router = router
