b30ba0de2f0de3f7fb9540578196f473f0838984
CoderAlchemy <1395822743@qq.com>
2020-07-05 14:21:25 +0800

    新增路由配置

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ const Koa = require('koa')
 
 const app = new Koa()
 
+const { router } = require('./routers')
+
 const PORT = process.env.PORT
 const HOST = process.env.HOST
+
+app.use(router.routes())
 
 app.listen(PORT, HOST, () => {
   console.log(`server start: http://${HOST}:${PORT}`)
