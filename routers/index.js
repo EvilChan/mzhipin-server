090775d58967f0b9eef5ca87c6430ebddd083e06
CoderAlchemy <1395822743@qq.com>
2020-07-05 16:43:58 +0800

    更新用户注册和登陆路由

--- routers/index.js
+++ routers/index.js
@@ -1,8 +1,16 @@
 const Router = require('koa-router')
+const md5 = require('blueimp-md5')
 
 const router = new Router({
   prefix: '/api'
 })
+
+const { UserModel } = require('../db/db_modules')
+
+const filter = {
+  password: 0,
+  __v: 0
+}
 
 const formatData = (...data) => {
   const res = {}
@@ -17,18 +25,41 @@ const formatData = (...data) => {
 router.post('/register', async (ctx, next) => {
   try {
     const { username, password, type } = ctx.request.body
-    ctx.body = formatData(0, { username, password, type })
+    const res = await UserModel.findOne({ username })
+    if (res) {
+      ctx.body = formatData(1, '此用户已存在')
+    } else {
+      const user = await new UserModel({
+        username,
+        type,
+        password: md5(password)
+      }).save()
+      ctx.cookies.set('userid', user._id, { maxAge: 1000 * 60 * 60 * 24 * 7 })
+      const data = { username, type, _id: user._id }
+      ctx.body = formatData(0, data)
+    }
   } catch (err) {
     ctx.body = formatData(1, '服务器错误')
+    console.error(err)
   }
 })
 
 router.post('/login', async (ctx, next) => {
   try {
     const { username, password } = ctx.request.body
-    ctx.body = formatData(0, { username, password })
+    const user = await UserModel.findOne(
+      { username, password: md5(password) },
+      filter
+    )
+    if (user) {
+      ctx.cookies.set('userid', user._id, { maxAge: 1000 * 60 * 60 * 24 * 7 })
+      ctx.body = formatData(0, user)
+    } else {
+      ctx.body = formatData(1, '用户名或密码错误')
+    }
   } catch (err) {
     ctx.body = formatData(1, '服务器错误')
+    console.error(err)
   }
 })
 
